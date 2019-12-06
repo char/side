87d8121dfb12c3d416bae46a1682a6a42d62f201
half cambodian hacker man <half-kh-hacker@hackery.site>
2019-12-06 11:31:18 +0000

    deploy.sh should also push

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@ rm -rf docs/build/*
 (cd docs/ && make)
 cp -r lib/ docs/build/x
 
-(cd docs/build/ && git add -A . && git commit -m "[Auto] Deploy $(date)")
+(cd docs/build/ && git add -A . && git commit -m "[Auto] Deploy $(date)" && git push)
