ffaf4ba405dae95c9fab3f9639ca111855792d4f
half cambodian hacker man <half-kh-hacker@hackery.site>
2019-12-24 18:53:30 +0000

    1.1.0: Add a 'writeText' function to BuildContext

---
+++ lib/x/@1.1.0/mod.ts
@@ -0,0 +1,6 @@
+export const version = "1.0.0";
+
+export * from "./build_context.ts";
+export * from "./util.ts";
+import * as ext from "./ext.ts";
+export { ext }
---
+++ lib/x/@1.1.0/util.ts
@@ -0,0 +1,24 @@
+const encoder = new TextEncoder();
+const decoder = new TextDecoder("utf-8");
+
+export const decodeUTF8 = (buffer: Uint8Array) => decoder.decode(buffer)
+export const encodeUTF8 = (string: string) => encoder.encode(string)
+
+export const withText: <T>(buffer: Uint8Array, callback: (s: string) => T) => T =
+  (buffer, callback) => callback(decodeUTF8(buffer))
+
+export function transformText<T extends (string | Promise<string>)>(buffer: Uint8Array, callback: (text: string) => T): Uint8Array | Promise<Uint8Array> {
+  const result = withText(buffer, callback)
+  if (result instanceof Promise) {
+    return result.then(encodeUTF8) as Promise<Uint8Array>
+  }
+
+  return encodeUTF8(result as string)
+}
+
+export function prettyLink(href: string) {
+  if (href.endsWith("/index.html") || href === "index.html")
+    href = href.substring(0, href.length - "index.html".length)
+
+  return href
+}
